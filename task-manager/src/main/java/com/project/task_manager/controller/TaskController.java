f8dfbf5369069a76061d0b70c6e2742c735e8820
Surya-prathap <13794prathap@gmail.com>
2026-05-26 22:13:30 +0530

    feat: Implemented BASIC CRUD Operations

--- task-manager/src/main/java/com/project/task_manager/controller/TaskController.java
+++ task-manager/src/main/java/com/project/task_manager/controller/TaskController.java
@@ -3,10 +3,9 @@ package com.project.task_manager.controller;
 import com.project.task_manager.entity.Task;
 import com.project.task_manager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -18,6 +17,26 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody Task task){
        return taskService.addTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Integer id){
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Integer id, @RequestBody Task updatedTask){
+        return taskService.updateTask(id,updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Integer id){
+        taskService.deleteTask(id);
     }
 
 }
--- task-manager/src/main/java/com/project/task_manager/service/TaskService.java
+++ task-manager/src/main/java/com/project/task_manager/service/TaskService.java
@@ -2,8 +2,11 @@ package com.project.task_manager.service;
 
 import com.project.task_manager.entity.Task;
 import com.project.task_manager.repository.TaskRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -13,5 +16,26 @@ public class TaskService {
 
     public Task addTask(Task task){
         return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Integer id){
+        return taskRepository.getById(id);
+    }
+
+    public Task updateTask(Integer id,Task updatedTask){
+        Task existingTask = taskRepository.getById(id);
+        existingTask.setTitle(updatedTask.getTitle());
+        existingTask.setDescription(updatedTask.getDescription());
+        existingTask.setStatus(updatedTask.isStatus());
+        existingTask.setDueDate(updatedTask.getDueDate());
+        return taskRepository.save(existingTask);
+    }
+
+    public void deleteTask(Integer id){
+        taskRepository.deleteById(id);
     }
 }
